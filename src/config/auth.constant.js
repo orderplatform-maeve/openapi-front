7c57cf978f2a3f5a7a3839e5a8b6c6b2fdd32d30
laon <cj4luv@gmail.com>
2020-03-30 14:23:42 +0900

    cookie 네임 수정

--- src/config/auth.constant.js
+++ src/config/auth.constant.js
@@ -1,7 +1,8 @@
-import { isDev } from '@utils/constants';
+// import { isDev } from '@utils/constants';
 
 const COOKIE_AUTH_NAME = 'auth';
 
-export const COOKIE_DOMAIN = isDev ? 'torder.co.kr' : null;
+// export const COOKIE_DOMAIN = isDev ? 'torder.co.kr' : null;
+export const COOKIE_DOMAIN = 'torder.co.kr';
 
 export default COOKIE_AUTH_NAME;
