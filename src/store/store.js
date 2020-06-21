9b703cb922e8ad8238f3722ea8b40597ffd7f8dc
Jaehun Cho <cj4luv@gmail.com>
2020-06-21 14:00:02 +0900

    소켓 모달 확인시 로컬에서도 값 변경 되게

--- src/store/store.js
+++ src/store/store.js
@@ -26,7 +26,8 @@ function imagePreload(url) {
 const socket = {
   mutations: {
     SOCKET_res(state, message) {
-      console.log(message);
+      // console.log(message);
+      message;
     },
     SOCKET_orderlog(state, order) {
       if (vaildShopCode(state, order)) {
