8df2832a6c0049edf919bae5cfd6b37c784f25c8
Jae-Hun Cho <cj4luv@gmail.com>
2021-10-25 16:15:02 +0900

    버젼 기록

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const isDemo = process.env.SERVER_TYPE === 'demo';
 export const isDev = process.env.NODE_ENV === 'development';
 
-export const version = 'v2.0.1';
+export const version = 'v2.0.2';
