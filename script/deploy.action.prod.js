081cf95fe9010fff3ca30887c80bf78f73339ed4
Jae-Hun Cho <cj4luv@gmail.com>
2021-11-09 18:45:21 +0900

    exec import 수정

--- script/deploy.action.prod.js
+++ script/deploy.action.prod.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const exec = require('@actions/exec');
+const { exec } = require('@actions/exec');
 const os = require('os');
 const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
