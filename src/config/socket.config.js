683ddf6bb6a7a90dc91f13c5da5f9bb82222c647
Jae-Hun Cho <cj4luv@gmail.com>
2022-11-16 18:35:37 +0900

    [edit] 소켓 개발망 운영망 분리 로직 추가

--- src/config/socket.config.js
+++ src/config/socket.config.js
@@ -3,10 +3,10 @@ import { IS_DEV_TEAM } from '@utils/constants';
 
 // const DEV_SOCKET_URL = 'localhost:3003';
 // const DEV_SOCKET_URL = 'http://dev-socket-v2.torder.co.kr';
-// const PROD_SOCKET_URL = 'http://master.socketio.torder.co.kr/';
 const DEV_SOCKET_URL = 'http://development.socket.torder.co.kr:3003';
+const PROD_SOCKET_URL = 'http://master.socketio.torder.co.kr';
 
-const SOCKET_URL = IS_DEV_TEAM ? DEV_SOCKET_URL : 'http://master.socketio.torder.co.kr';
+const SOCKET_URL = IS_DEV_TEAM ? DEV_SOCKET_URL : PROD_SOCKET_URL;
 
 export default {
   debug: false,
