d7ae7450447af78eea91799da00d86ad8dd17f57
torder_pmk <pmk1@torder.co.kr>
2023-03-09 19:06:13 +0900

    [edit] 페이지네이션 연결

--- src/apis/credit.js
+++ src/apis/credit.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import endpoints from '@apis/endpoints';
 
 const requestPayDetails = (config) => {
-  const url = `${endpoints.credit.payDetails}?page=${config.page}&size=${config.size}&storeCode=${config.storeCode}`;
+  const url = `${endpoints.credit.payDetails}?page=${config.page}&size=${config.size}&storeCode=${config.storeCode}&tabletNumber=${config.tabletNumber}&paymentMethod=${config.paymentMethod}&paymentStatus=${config.paymentStatus}&startDatetime=${config.startDatetime}&endDatetime=${config.endDatetime}`;
   const auth = {
     auth: {
       username: 'torder-credit-api',
--- src/router/views/PaymentDetails.vue
+++ src/router/views/PaymentDetails.vue
@@ -28,25 +28,29 @@
       p 카드 번호
       p 결제 일시
       p 주문접수 상태
-      p 주문 일시
       p 자세히
-    .fixed-credit-information(v-for="item in paymentList" :key="item.id")
-      p {{ item.no }}
-      p {{ item.tabletNumber }}
-      p {{ item.paymentMethod }}
-      p {{ item.paymentStatus }}
-      button {{ item.paymentConfirmation }} 미수금
-      p {{ item.amount }}
-      p {{ item.acquirer }}
-      p {{ item.cardNumber }}
-      p {{ item.approvalDatetime }}
-      p {{ item.orderStatus }}
-      p {{ item.orderDatetime }}
-      button 자세히
+    .credit-info-wrap
+      .fixed-credit-information(v-for="item in paymentList" :key="item.id")
+        p {{ item.no }}
+        p {{ item.tabletNumber }}
+        p {{ item.paymentMethod }}
+        p {{ item.paymentStatus }}
+        button.check-payment {{ item.paymentConfirmation }}
+        p {{ item.amount.toLocaleString() }}
+        p {{ item.acquirer }}
+        p {{ item.cardNumber }}
+        p {{ item.approvalDatetime }}
+        p {{ item.orderStatus }}
+        button.detail-button 자세히
   .wrap-pagination
-    button.previous-button
-    button.page-number
-    button.next-button
+    button.previous-button(v-if="showPageArrow" @click="clickPrevPage()") <
+    button.page-block(
+      v-for="num in pagination.list"
+      :key="num"
+      :class="getActivePageStyle(num)"
+      @click="getPaysDetails(num)"
+      ) {{ num }}
+    button.next-button(v-if="showPageArrow" @click="clickNextPage()") >
 
 </template>
 
@@ -65,35 +69,98 @@ export default {
         totalElementCount: 0, // 총 검색 갯수
         totalPageCount: 0, // 총 페이지 수
       },
+      pagination: {
+        list: [],
+        pageIndex: 0,
+        size: 10,
+        startPage: 0,
+        endPage: 0,
+      }
     };
   },
   computed: {
+    // 페이지 양 옆 화살표 버튼
+    showPageArrow() {
+      return this.pageInfo.totalPageCount > this.pagination.size;
+    },
   },
   methods: {
-    async getPaysDetails() {
+    // 페이지네이션
+    loadPagination() {
+      const { currentPageNo, totalPageCount } = this.pageInfo;
+      const { pagination } = this;
+
+      // 조회하려는 현재 페이지의 소속된 페이지 그룹 위치 계산
+      pagination.pageIndex = Math.ceil(currentPageNo / pagination.size);
+
+      // 페이지 그룹의 마지막 페이지
+      pagination.endPage = currentPageNo + Math.ceil(pagination.size / 2);
+      if (pagination.endPage > totalPageCount) {
+        pagination.endPage = totalPageCount;
+      }
+      else if (pagination.endPage < pagination.size) {
+        pagination.endPage = pagination.size;
+      }
+      // 페이지 그룹의 첫 페이지
+      pagination.startPage = pagination.endPage - (pagination.size - 1);
+      if (pagination.startPage < 1) {
+        pagination.startPage = 1;
+      }
+
+      // 현재 페이지 그룹만 배열로 생성
+      pagination.list = [];
+      for (let i = pagination.startPage; i <= pagination.endPage; i++) {
+        pagination.list.push(i);
+      }
+    },
+    clickNextPage() {
+      if (this.pageInfo.totalPageCount > this.pageInfo.currentPageNo) {
+        const plus = this.pageInfo.currentPageNo + 1;
+        this.getPaysDetails(plus);
+      }
+    },
+    clickPrevPage() {
+      if (this.pageInfo.currentPageNo > 1) {
+        const minus = this.pageInfo.currentPageNo - 1;
+        this.getPaysDetails(minus);
+      }
+    },
+    async getPaysDetails(page) {
       try {
         const config = {
-          page : 0,
-          size : 30,
+          page: page - 1,
+          size: 1,
           storeCode: this.$store.state.auth.store.store_code,
+          tabletNumber: '',
+          paymentMethod: '',
+          paymentStatus: '',
+          startDatetime: '',
+          endDatetime: '',
         };
         const res = await requestPayDetails(config);
         if (res.data.resultCode === 200) {
           this.paymentList = res.data.resultData.content;
-          this.pageInfo.currentPageNo = res.data.resultData.currentPageNo;
+          this.pageInfo.currentPageNo = res.data.resultData.currentPageNo + 1;
           this.pageInfo.elementSizePerPage = res.data.resultData.elementSizePerPage;
           this.pageInfo.numberOfElement = res.data.resultData.numberOfElement;
           this.pageInfo.totalElementCount = res.data.resultData.totalElementCount;
           this.pageInfo.totalPageCount = res.data.resultData.totalPageCount;
-          console.log(this.paymentList, this.pageInfo);
+
+          this.loadPagination();
         }
       } catch (error) {
         console.log(error);
       }
+    },
+    getActivePageStyle(current) {
+      return {
+        'page-number': true,
+        'pagination-active': this.pageInfo.currentPageNo === current,
+      };
     }
   },
   mounted() {
-    this.getPaysDetails();
+    this.getPaysDetails(1);
   },
 };
 </script>
@@ -152,7 +219,6 @@ export default {
   .wrap-credit-information-table {
     position: relative;
     height: calc(100vh - 15.875025vw);
-    overflow-y: auto;
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
@@ -160,12 +226,11 @@ export default {
     margin-top: 1.5625vw !important;
 
     .fixed-credit-information-header {
-      /* display: flex;
-      justify-content: space-between; */
       display: grid;
-      grid-template-columns: 34px 50px 30px 39px 51px 53px 120px 80px 80px 84px 80px 51px;
-      /* grid-template-columns: repeat(11, auto); */
+      grid-template-columns: 2.6563vw 3.9063vw 2.3438vw 3.0469vw 11.71875vw 4.2969vw 9.3750vw 6.2500vw 6.2500vw 6.5625vw 4.6875vw;
       justify-content: space-between;
+      align-items: center;
+
       border-bottom: solid 0.078125vw #ccc;
 
       > p {
@@ -176,14 +241,15 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+        text-align: center;
       }
     }
-    .fixed-credit-information {
-      /* display: grid;
-      grid-template-columns: 6.25vw 6.25vw 11.71875vw 9.375vw 7.8125vw 10.9375vw; */
+
+    .credit-info-wrap {
+      overflow-y: scroll;
+      .fixed-credit-information {
       display: grid;
-      grid-template-columns: 34px 50px 30px 39px 51px 53px 120px 80px 80px 84px 80px 51px;
-      /* grid-template-columns: repeat(11, auto); */
+      grid-template-columns: 2.6563vw 3.9063vw 2.3438vw 3.0469vw 11.71875vw 4.2969vw 9.3750vw 6.2500vw 6.2500vw 6.5625vw 4.6875vw;
       column-gap: 5px;
       justify-content: space-between;
       align-items: center;
@@ -198,26 +264,33 @@ export default {
         align-items: center;
         text-align: center;
       }
-
-      .status {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        box-sizing: border-box;
-
-        > button {
-          font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-          font-size: 1.25vw;
-          letter-spacing: -0.03125vw;
-          color: #fc0000;
-          width: 100%;
-          height: 2.8125vw;
-          background-color: #fff;
-          border: solid 0.078125vw #fc0000;
-          border-radius: 0.390625vw;
-        }
+      .check-payment {
+        font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+        font-size: 1.25vw;
+        letter-spacing: -0.03125vw;
+        color: #fc0000;
+        width: 100%;
+        height: 2.8125vw;
+        background-color: #fff;
+        border: solid 0.078125vw #fc0000;
+        border-radius: 0.390625vw;
       }
+
+      .detail-button {
+        font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+        font-size: 1.25vw;
+        letter-spacing: -0.03125vw;
+        color: #fff;
+        width: 100%;
+        height: 2.8125vw;
+        background-color: #000;
+        border: solid 0.078125vw #000;
+        border-radius: 0.390625vw;
+      }
+
     }
+    }
+
   }
   .wrap-pagination {
     height: 5vw;
@@ -226,7 +299,7 @@ export default {
     align-items: center;
     gap: 0.78125vw;
 
-    > button {
+    .page-block {
       width: 2.65625vw;
       height: 2.65625vw;
       font-family: 'Spoqa Han Sans Neo', 'sans-serif';
@@ -237,18 +310,25 @@ export default {
       border-radius: 0.390625vw;
     }
 
-    .paginationActive {
+    .pagination-active {
       color: #fff;
       background-color: #000;
     }
 
     .previous-button,
     .next-button {
+      width: 2.65625vw;
+      height: 2.65625vw;
+      font-family: 'Spoqa Han Sans Neo', 'sans-serif';
       display: flex;
       justify-content: center;
       align-items: center;
       font-size: 2vw;
       font-weight: 400;
+      color: #12151d;
+      background-color: #fff;
+      border: solid 0.078125vw #ccc;
+      border-radius: 0.390625vw;
     }
   }
 }
