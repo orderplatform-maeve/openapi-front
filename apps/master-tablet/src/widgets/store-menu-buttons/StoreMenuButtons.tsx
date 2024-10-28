3666aca3874af25b640669ab2a093dca7119da93
orderplatform-jaegeun <jaegeun@torder.com>
2024-10-28 19:05:55 +0900

    fix: [ODPLFE-367] 좌측메뉴의 active 상태를 위한 url 체크시 base path 만 바라보도록 수정

--- apps/master-tablet/src/widgets/store-menu-buttons/StoreMenuButtons.tsx
+++ apps/master-tablet/src/widgets/store-menu-buttons/StoreMenuButtons.tsx
@@ -6,18 +6,19 @@ import styles from './StoreMenuButtons.module.css';
 export const StoreMenuButtons = () => {
   const router = useRouterState();
   const path = router.location.pathname;
+  const basePath = path.split('/')[1];
   return (
     <section className={styles.container}>
-      <LinkButton size={'medium'} link={'/notice'} color={path === '/notice' ? 'primary' : 'basic'}>
+      <LinkButton size={'medium'} link={'/notice'} color={basePath === 'notice' ? 'primary' : 'basic'}>
         <span>공지사항</span>
       </LinkButton>
-      <LinkButton size={'medium'} link={'/order'} color={path === '/order' ? 'primary' : 'basic'}>
+      <LinkButton size={'medium'} link={'/order'} color={basePath === 'order' ? 'primary' : 'basic'}>
         <span>주문보기</span>
       </LinkButton>
-      <LinkButton size={'medium'} link={'/additional'} color={path === '/additional' ? 'primary' : 'basic'}>
+      <LinkButton size={'medium'} link={'/additional'} color={basePath === 'additional' ? 'primary' : 'basic'}>
         <span>추가기능</span>
       </LinkButton>
-      <LinkButton size={'medium'} link={'/payment'} color={path === '/payment' ? 'primary' : 'basic'}>
+      <LinkButton size={'medium'} link={'/payment'} color={basePath === 'payment' ? 'primary' : 'basic'}>
         <span>결제내역</span>
       </LinkButton>
     </section>
