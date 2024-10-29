02c95c8022fd59584e7634f0630e558d82889f97
orderplatform-jaegeun <jaegeun@torder.com>
2024-10-29 17:02:03 +0900

    refactor: [ODPLFE-367] 불필요한 타입정의영역 삭제

--- apps/master-tablet/src/App.tsx
+++ apps/master-tablet/src/App.tsx
@@ -8,13 +8,6 @@ import { routeTree } from './routeTree.gen';
 const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router;
-  }
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
