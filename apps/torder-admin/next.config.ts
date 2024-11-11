29c94afacf5bc04a899e0a6b13a167c5242860b6
orderplatform-maeve <maeve@torder.com>
2024-11-11 16:41:44 +0900

    feat: [ODPLFE-467]: packages/admin-ui/ 제거

--- apps/torder-admin/next.config.ts
+++ apps/torder-admin/next.config.ts
@@ -1,7 +1,7 @@
 import withTM from 'next-transpile-modules';
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = withTM(['@torder/admin-ui', '@torder/client-fetcher', '@torder/ui', '@torder/utils'])({
+const nextConfig: NextConfig = withTM(['@torder/client-fetcher', '@torder/ui', '@torder/utils'])({
   /* config options here */
   reactStrictMode: true,
   env: {
--- packages/admin-ui/eslint.config.js
+++
@@ -1,5 +0,0 @@
-import reactEsLint from "@torder/eslint-config-9/react.eslint.config.js"
-
-export default [
-  ...reactEsLint,
-];
